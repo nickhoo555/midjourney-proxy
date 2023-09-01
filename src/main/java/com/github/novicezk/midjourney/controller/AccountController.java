7319cb1d28c174fc05d485f7f6f7116abc36f830
NickHoo <nickhoo555@gmail.com>
2023-09-01 04:21:28 +0000

    换皮

--- src/main/java/com/github/novicezk/midjourney/controller/AccountController.java
+++ src/main/java/com/github/novicezk/midjourney/controller/AccountController.java
@@ -15,14 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.ArrayList;
 
-@Api(tags = "账号查询")
+// @Api(tags = "账号查询")
 @RestController
 @RequestMapping("/account")
 @RequiredArgsConstructor
 public class AccountController {
 	private final DiscordLoadBalancer loadBalancer;
 
-	@ApiOperation(value = "指定ID获取账号")
+	// @ApiOperation(value = "指定ID获取账号")
 	@GetMapping("/{id}/fetch")
 	public DiscordAccount fetch(@ApiParam(value = "账号ID") @PathVariable String id) {
 		
@@ -31,7 +31,7 @@ public class AccountController {
 		return null;
 	}
 
-	@ApiOperation(value = "查询所有账号")
+	// @ApiOperation(value = "查询所有账号")
 	@GetMapping("/list")
 	public List<DiscordAccount> list() {
 		return new ArrayList<>();
