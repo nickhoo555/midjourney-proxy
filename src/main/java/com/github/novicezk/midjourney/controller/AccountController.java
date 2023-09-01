6ae0c3a4af2400a38342a31cadd5803800505ab1
NickHoo <nickhoo555@gmail.com>
2023-09-01 03:50:58 +0000

    禁用 账号查询

--- src/main/java/com/github/novicezk/midjourney/controller/AccountController.java
+++ src/main/java/com/github/novicezk/midjourney/controller/AccountController.java
@@ -14,23 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Api(tags = "账号查询")
+//@Api(tags = "账号查询")
 @RestController
 @RequestMapping("/account")
 @RequiredArgsConstructor
 public class AccountController {
 	private final DiscordLoadBalancer loadBalancer;
 
-	@ApiOperation(value = "指定ID获取账号")
+	//@ApiOperation(value = "指定ID获取账号")
 	@GetMapping("/{id}/fetch")
 	public DiscordAccount fetch(@ApiParam(value = "账号ID") @PathVariable String id) {
-		DiscordInstance instance = this.loadBalancer.getDiscordInstance(id);
-		return instance == null ? null : instance.account();
+		return null
+		// DiscordInstance instance = this.loadBalancer.getDiscordInstance(id);
+		// return instance == null ? null : instance.account();
 	}
 
-	@ApiOperation(value = "查询所有账号")
+	//@ApiOperation(value = "查询所有账号")
 	@GetMapping("/list")
 	public List<DiscordAccount> list() {
-		return this.loadBalancer.getAllInstances().stream().map(DiscordInstance::account).toList();
+		return null
+		// return this.loadBalancer.getAllInstances().stream().map(DiscordInstance::account).toList();
 	}
 }
